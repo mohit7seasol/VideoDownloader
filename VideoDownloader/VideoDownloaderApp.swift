e7d2f53c6806a5d8cc4cd896682e4c0a738045ad
mohit7seasol <mohit.7seasol@gmail.com>
2026-03-09 14:59:06 +0530

    added project

---
+++ VideoDownloader/VideoDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoDownloaderApp.swift
+//  VideoDownloader
+//
+//  Created by DREAMWORLD on 09/03/26.
+//
+
+import SwiftUI
+
+@main
+struct VideoDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
