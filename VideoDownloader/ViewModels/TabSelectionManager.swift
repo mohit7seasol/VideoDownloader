8b045aaa7b698f475c09c4160077d689f2312717
mohit7seasol <mohit.7seasol@gmail.com>
2026-03-23 10:05:36 +0530

    change tabbar index

--- VideoDownloader/ViewModels/TabSelectionManager.swift
+++ VideoDownloader/ViewModels/TabSelectionManager.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import Combine
 
 class TabSelectionManager: ObservableObject {
-    @Published var selectedTab: Int = 0 // Changed default to HomeView (index 0)
+    @Published var selectedTab: Int = 1 // Changed default to HomeView (index 0)
     
     func navigateToHistory() {
         selectedTab = 2
